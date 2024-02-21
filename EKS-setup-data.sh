ef69573fde3f512c5a564cba462078069eadd2d3
Ritesh <riteshverma388@gmail.com>
2024-02-21 19:16:18 +0530

    test_commit

---
+++ EKS-setup-data.sh
@@ -0,0 +1,63 @@
+# Chroot with alationadmin
+AlationAdminUser="/bin/su - alationadmin -c"
+# Chroot with alation
+AlationUser="/bin/su - alation -c"
+# Chroot wihtout specify user
+ChrootBase="chroot /opt/alation/alation"
+kubectltimeout="600"
+#CMD="alation_conf alation.authentication.builtin.enabled"
+CMD="Select ocfconfig.ds_id, connector.name as connector_name, textarea.key as param_name, textarea.value as param_value from rosemeta_datasource ds inner join rosemeta_ocfconfiguration ocfconfig on ds.id = ocfconfig.ds_id inner join connector_metadata_connector connector on ocfconfig.connector_id = connector.id inner join connector_metadata_textarea textarea on ocfconfig.connector_configuration_id=textarea.configuration_id where textarea.key in ('custom_qli_query') and textarea.value != '' and textarea.value is not null and ds.deleted = false and connector.name like  '%Oracle OCF%';"
+OUTPUT_FILE_NAME=eks-st-CMS-7100.txt
+
+
+
+if [ "$#" -lt 1 ]; then
+    echo "Error: Please provide the file with space sprated vaules"
+    exit 1  # Exit the script with an error status
+fi
+
+	
+clusterlist="$1"
+
+RunCommand () {
+        Region=$1
+        Cluster=$2
+
+        echo "$1 $2"
+        POD_NAME=$(kubectl get pods | grep "^alationfc" | grep -v ha| grep Running)
+        if [ -z "$POD_NAME" ]; then
+                echo "alationfc pod not found in $Cluster - $Region."
+        else
+                echo "restart celery beat"
+                Data=$(kubectl --request-timeout $kubectltimeout exec $POD_NAME -c alationfc -- $ChrootBase $AlationUser  "echo \"${query}\" | alation_psql"  < /dev/null )
+                if [ -z "$Data" ]; then
+                        echo "failed to run the script on $Cluster - $Region - $POD_NAME "
+                else
+                        #Value=$(echo "$Data"| cut -d "=" -f2)
+                        echo "running on $Cluster - $Region  "
+
+                        echo -e "Cluster: $Cluster $Region\n $Data\n ======= \n" >> $OUTPUT_FILE_NAME
+                        #echo  "$Data" >> $Region-$Cluster.txt
+                fi
+        fi
+       }
+
+
+
+while IFS= read -r line; do
+        read -r Region Name <<< "$line"
+        aws eks describe-cluster --name $Name --region $Region  | grep "Environment" | grep "prod" > /dev/null
+        echo "name $Name --region $Region"
+        TAG_STATUS=$?
+        if [ $TAG_STATUS -eq 0 ]; then
+                aws eks --region $Region update-kubeconfig --name $Name --role-arn arn:aws:iam::118618885326:role/AlationEKSAdminRole
+                RunCommand $Region $Name
+        else
+            echo -e "Cluster is not from PROD Environment"
+        fi
+
+
+
+done < "$clusterlist"
+
+$$$
---
+++ changing_zeus_tenant.sh
@@ -0,0 +1,134 @@
+Need to find the values of the following settings for the prod customers on CNA (All regions)
+
+
+Alation conf value of 
+
+alation.authentication.builtin.enabled
+
+alation.authentication.ldap.enabled
+
+alation.authentication.saml.enabled
+
+alation.authentication.oidc.enabled
+
+Output of SiteSettings.get('account_policy').get('moderate_signups') run from alation_django_shell
+
+
+#!/bin/bash
+​
+# Exit on error.
+#set -o errexit
+# Exit on error inside any functions or subshells.
+#set -o errtrace
+# Catch the errors eg. in case mysqldump fails (but gzip succeeds) in `mysqldump |gzip`
+#set -o pipefail
+# Do not allow use of undefined vars. Use ${VAR:-} to use an undefined VAR
+#set -o nounset
+# Turn on traces, useful while debugging but commented out by default
+# Turn on if needed for debugging.
+#set -o xtrace
+#set -e
+​
+# Chroot with alationadmin
+alationadmin_user="/bin/su - alationadmin -c"
+# Chroot with alation
+alation_user="/bin/su - alation -c"
+# Chroot with alation psql util
+alation_psql_util="alation_pgsql_util run_psql"
+# Chroot with alation
+alation_user="/bin/su - alation -c"
+# Chroot wihtout specify user
+chroot_base="chroot /opt/alation/alation"
+​
+OUTPUT_FILE_NAME="zeus-bulk-query-output.txt"
+​
+​
+​
+​
+query1="SELECT DISTINCT ds.id AS ds_id, title, connector.name FROM rosemeta_datasource ds LEFT JOIN rosemeta_ocfconfiguration conf ON conf.ds_id = ds.id LEFT JOIN connector_metadata_connector connector ON conf.connector_id = connector.id;
+"
+​
+#aws ec2 describe-regions --region eu-west-1 | grep "RegionName" | awk '{ print $2 }' | tr -d '",' > regions.txt
+​
+​
+# for i in `cat regions.txt`; do
+#     echo $i
+#     aws eks list-clusters --region $i | grep prod | tr -d '",' | tr -d " " > eks-clusters-$i.txt
+# done
+​
+if [[ -f $OUTPUT_FILE_NAME ]]; then
+    rm -f $OUTPUT_FILE_NAME
+fi
+​
+read -p "Please Confirm If Zeus KUBECONFIG Exported [Y/N] :" ZEUS_KUBECONFIG
+​
+if [ "$ZEUS_KUBECONFIG" = "Y" ]; then
+​
+    kubectl config get-contexts | grep -E '\bprod' | grep prod-enterprise-use1 | awk '{ print $2 }' > zeus-cluster-contexts.txt
+    # ps -ef | grep port-forward | grep kubectl
+    # if [ $? -eq 0 ]; then
+    #     PROCESS_ID=`ps -ef | grep port-forward | grep kubectl | awk '{ print $2 }'`
+    #     kill -9 $PROCESS_ID
+    # fi
+    # kubectl port-forward service/tenant-resource-catalog 8001:80 &
+    # echo -e "\n"
+    # sleep 12
+    # echo -e "\n Fetching All TENANTS ID in All PROD ZEUS clusters"
+    # sleep 6
+​
+    for i in `cat zeus-cluster-contexts.txt`; do
+​
+        ps -ef | grep port-forward | grep kubectl
+        if [ $? -eq 0 ]; then
+            PROCESS_ID=`ps -ef | grep port-forward | grep kubectl | awk '{ print $2 }'`
+            kill -9 $PROCESS_ID
+        fi
+        echo -e "\n Switching Context Name : $i"
+        kubectl config use-context $i
+        kubectl port-forward service/tenant-resource-catalog 8001:80 &
+        echo -e "\n Port Forwarding Configuring ....\n"
+        sleep 15
+        echo -e "\n Fetching All TENANTS ID in ZEUS cluster - $i\n"
+        sleep 6
+        echo -e "\n Set Cluster Context to : $i"
+        sleep 5
+​
+        curl -X 'GET' 'http://localhost:8001/resourceSets?status=assigned' -H 'accept: application/json' | jq -r .resource_sets[].tenant_id > $i-tenants-id.txt
+​
+        for j in `cat $i-tenants-id.txt`;do
+            echo "********************************** Tenant-ID: $j *******************************"
+            echo -e "\n\n\n"
+            kubectl get deployment -n $j | grep "^alationfc"  > /dev/null
+            pod_status=$?
+            if [ $pod_status -eq 0 ]; then
+                echo -e "\n\n**********************  Checking pod Health on $j ********************\n\n"
+                kubectl exec -it deploy/alationfc -n $j -- hostname
+                pod_health=$?
+                if [ $pod_health -ne 0 ]; then
+                    echo "$j, exec not working on alationfc, $j"
+                else
+                    for n in {1..1}; do
+                        query=query$n
+                        echo "${!query}"
+                        echo -e "------------------------------------- Tenant iD : $j ------------------------------------- \n"
+                        echo -e "\n\n--------------------------------- Tenant iD : $j ------------------------------------- \n" >> $OUTPUT_FILE_NAME
+                        result=$(kubectl exec -it deploy/alationfc -n $j -- chroot /opt/alation/alation /bin/su - alation -c "echo \"${!query}\" | alation_psql")
+                        echo $result
+                        echo -e "$result"  >> $OUTPUT_FILE_NAME
+                        echo -e "\n\n================================================================================================\n"
+                        echo -e "\n\n================================================================================================\n" >> $OUTPUT_FILE_NAME
+                    done
+                fi
+            fi
+        done
+    done
+fi
+
+
+ 026c51db-1e43-4274-978f-eb401b2ec893
+ 
+ kubectl exec -it deploy/alationfc -n 026c51db-1e43-4274-978f-eb401b2ec893 -- chroot /opt/alation/alation /bin/su - alation -c "echo \"${!query1}\" | alation_psql"
+ 
+ result1=$(kubectl exec -it deploy/alationfc -n $j -- chroot /opt/alation/alation /bin/su - alation -c "alation_conf base_url")
+
+ ###
